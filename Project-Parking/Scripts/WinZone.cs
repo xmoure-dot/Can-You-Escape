7491bccc58073bdc45d1d6549b8f12ba15098600
Xmoure <79313865+xmoure-dot@users.noreply.github.com>
2021-04-11 18:28:11 +0300

    Add files via upload

---
+++ Project-Parking/Scripts/WinZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinZone : MonoBehaviour {
+
+
+	public GameObject winText;
+
+
+	void Start () {
+		
+		winText.gameObject.SetActive (false);
+	}
+
+	
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		
+		winText.gameObject.SetActive (true);
+	}
+	
+	
+}
---
+++ Project-Parking/Scripts/Αστο για το τελος/TouchDrag.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDrag : MonoBehaviour
+{
+    
+    float deltaX, deltaY;
+
+    static public bool move = true; //to kanoume static oste na mporoume na to kalesoume kai apo allo script.
+		
+    bool ColTouched = false;
+	
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+		
+        rb = GetComponent<Rigidbody2D> ();
+		
+        rb.isKinematic = true; //Gia min askeite kamia dinami epano tou 
+    }
+
+    void Update()
+    {
+		
+        if (Input.touchCount > 0) {
+			
+            Touch touch = Input.GetTouch (0);
+				
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint (touch.position);
+				
+            switch (touch.phase) {
+					
+	            
+                case TouchPhase.Began:
+						
+	                
+	                
+                    if (GetComponent <Collider2D> () == Physics2D.OverlapPoint (touchPos)) {
+							
+                        
+	                    ColTouched = true;
+							
+                     
+							
+                        rb.isKinematic = false;
+
+                        // touch offset
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                    }
+                    break;
+
+					
+                case TouchPhase.Moved:
+
+						
+                    if (GetComponent<Collider2D> () == Physics2D.OverlapPoint (touchPos) && ColTouched && move)
+	                    
+                        // Diorthosi apo to offset
+                        rb.MovePosition (new Vector2 (touchPos.x - deltaX,
+                            touchPos.y - deltaY));
+                    break;
+
+                
+                case TouchPhase.Ended:
+	                
+                    ColTouched = false;
+						
+                    rb.isKinematic = true;
+                    break;
+            }
+        }
+    }
+    
+    
+    
+}
